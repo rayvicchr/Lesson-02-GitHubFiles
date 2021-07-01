b59964ac2b4967b96d00e6ce9b75b613e795f89b
rayvicchr <rayvchrist@gmail.com>
2021-07-01 17:54:09 +0000

    I am pushing all the files to my Github

---
+++ C_Program.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main(){
+
+printf("Hello! I am a C-Program. Thank you!");
+
+return0;
+
+}
---
+++ HelloJava.java
@@ -0,0 +1,9 @@
+Class HelloJava {
+
+public static void main(string args{}){
+
+System.out.Prinln(" I am your Java Program. Thank you!");
+
+	}
+
+}
